690868fba9c4e8f7e6eb9a153690a4cdc6312d70
Jakub Wiraszka <WirachaPL@wp.pl>
2022-05-22 16:05:13 +0200

    List 10 DFS and BFS

--- src/Lista10/src/Graph.java
+++ src/Lista10/src/Graph.java
@@ -1,45 +1,46 @@
 package Lista10.src;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class Graph<T> {
     private final HashMap<T, Integer> dictionary;
-    private int nodeAmount;
-    private int[][] neighboursMatrix;
+    private HashMap<T, Boolean> nodesVisited;
+    private int nodesAmount;
+    private Edge<T>[][] neighboursMatrix;
 
     public Graph(List<Edge<T>> edges) {
-        // TODO: Przekształcenie krawędzi na macierz sąsiedztwa, odwzorowanie wierzchołka na indeks, itp.
         dictionary = new HashMap<>();
-        nodeAmount = 0;
-        neighboursMatrix = new int[10][10];
-        for(Edge<T> edge: edges) {
+        nodesAmount = 0;
+        neighboursMatrix = (Edge<T>[][]) new Edge[10][10];
+        for (Edge<T> edge : edges) {
             T source = edge.getSource();
             T destination = edge.getDestination();
             addNode(source);
             addNode(destination);
             handleFullMatrix();
-            neighboursMatrix[getIndex(source)][getIndex(destination)] = edge.getWeight();
+            neighboursMatrix[getIndex(source)][getIndex(destination)] = edge;
         }
     }
 
     private void addNode(T node) {
-        if(!dictionary.containsKey(node)) {
-            dictionary.put(node, nodeAmount);
-            nodeAmount++;
+        if (!dictionary.containsKey(node)) {
+            dictionary.put(node, nodesAmount);
+            nodesAmount++;
         }
     }
 
     private int getIndex(T node) {
-        return dictionary.get(node);
+        if (dictionary.containsKey(node)) {
+            return dictionary.get(node);
+        }
+        return -1;
     }
 
     private void handleFullMatrix() {
-        if(nodeAmount >= neighboursMatrix.length) {
+        if (nodesAmount >= neighboursMatrix.length) {
             int length = neighboursMatrix.length * 2;
-            int[][] newMatrix = new int[length][length];
-            for(int i = 0; i < neighboursMatrix.length; i++) {
+            Edge<T>[][] newMatrix = new Edge[length][length];
+            for (int i = 0; i < neighboursMatrix.length; i++) {
                 System.arraycopy(neighboursMatrix[i], 0, newMatrix[i], 0, neighboursMatrix.length);
             }
             neighboursMatrix = newMatrix;
@@ -47,17 +48,115 @@ public class Graph<T> {
     }
 
     public String depthFirst(T startNode) throws NoSuchElementException {
-        // TODO: Przejście przez graf metodą najpierw-wgłąb od podanego wierzchołka
-        return "";
+        return listToString(getDFS(startNode));
+    }
+
+    private List<T> getDFS(T startNode) {
+        checkIndex(startNode);
+        nodesVisited = new HashMap<>();
+        List<T> nodes = new LinkedList<>();
+
+        DFS(startNode, nodes);
+
+        return nodes;
+    }
+
+    private void DFS(T node, List<T> nodes) {
+        if (isVisited(node)) {
+            return;
+        }
+        nodes.add(node);
+        visitNode(node);
+        for (T i : getSortedNeighbours(node)) {
+            DFS(i, nodes);
+        }
+    }
+
+    private boolean isVisited(T node) {
+        if (!nodesVisited.containsKey(node)) {
+            nodesVisited.put(node, false);
+        }
+
+        return nodesVisited.get(node);
+    }
+
+    private void visitNode(T node) {
+        nodesVisited.put(node, true);
+    }
+
+    private void checkIndex(T startNode) {
+        if (getIndex(startNode) == -1) {
+            throw new NoSuchElementException();
+        }
     }
 
     public String breadthFirst(T startNode) throws NoSuchElementException {
-        // TODO: Przejście przez graf metodą najpierw-wszerz od podanego wierzchołka
-        return "";
+        return listToString(getBFS(startNode));
+    }
+
+    private List<T> getBFS(T startNode) {
+        checkIndex(startNode);
+        nodesVisited = new HashMap<>();
+        List<T> nodes = new LinkedList<>();
+        List<T> nodesToVisit = new LinkedList<>();
+        nodesToVisit.add(startNode);
+
+        while (!nodesToVisit.isEmpty()) {
+            T currentNode = nodesToVisit.get(0);
+            nodesToVisit.addAll(getSortedNeighbours(currentNode));
+            if (!isVisited(currentNode)) {
+                nodes.add(currentNode);
+                visitNode(currentNode);
+            }
+            nodesToVisit.remove(currentNode);
+        }
+
+        return nodes;
+    }
+
+    private List<T> getSortedNeighbours(T node) {
+        TreeSet<Edge<T>> neighbours = new TreeSet<>(new EdgesComparator());
+        List<T> result = new LinkedList<>();
+
+        for (Edge<T> edge : neighboursMatrix[getIndex(node)]) {
+            if (edge != null) {
+                if (!isVisited(edge.getDestination())) {
+                    neighbours.add(edge);
+                }
+            }
+        }
+        for (Edge<T> edge : neighbours) {
+            result.add(edge.getDestination());
+        }
+
+        return result;
     }
 
     public int connectedComponents() {
         // TODO: Liczba składowych spójnych grafu
         return -1;
+    }
+
+    private String listToString(List<T> nodes) {
+        StringBuilder sb = new StringBuilder();
+
+        for (T node : nodes) {
+            sb.append(node.toString()).append(", ");
+        }
+
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    private class EdgesComparator implements Comparator<Edge<T>> {
+        @Override
+        public int compare(Edge<T> o1, Edge<T> o2) {
+            if (o1.getWeight() < o2.getWeight()) {
+                return -1;
+            } else if (o1.getWeight() > o2.getWeight()) {
+                return 1;
+            } else {
+                return o1.hashCode() - o2.hashCode();
+            }
+        }
     }
 }
